9b4e31cf1c4d9c5a4135b9c9710182e31302e581
Aleksandr Moskalyk <alex.moskalik@gmail.com>
2021-08-30 20:59:39 +0300

    add colorTheme

--- src/js/menu-function.js
+++ src/js/menu-function.js
@@ -6,7 +6,7 @@ const container = document.querySelector('.js-menu')
 
 
 const allCards = data.map((itemCard) => menuCardTemplate(itemCard)).join('');
-console.log(allCards);
+
 
 
 container.insertAdjacentHTML("beforeend", allCards)
@@ -22,5 +22,24 @@ const bodyColor = document.querySelector('body');
 themeBtn.addEventListener('change', handleColorThemeChanger);
 
 function handleColorThemeChanger(e) {
-    
+    const themeColor = e.target.checked ? Theme.DARK : Theme.LIGHT;
+    setData(themeColor)
+    setBodyColor(themeColor)
 }
+
+function setData (data) {
+localStorage.setItem('theme', data)
+    
+};
+
+function setBodyColor(classColor) {
+    bodyColor.className=classColor
+}
+
+function start() {
+    const fistData = localStorage.getItem('theme') || Theme.LIGHT;
+    setBodyColor(fistData);
+    themeBtn.checked = fistData === Theme.DARK;
+}
+
+start();
